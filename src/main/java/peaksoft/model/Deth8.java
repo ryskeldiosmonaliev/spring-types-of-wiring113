36d3733742516317b50d0ced48c84f8dc5fb3b5c
Рыскелди <ryskeldi.osmonaliev@mail.ru>
2022-10-20 16:57:20 +0300

    comit

--- src/main/java/peaksoft/model/Deth8.java
+++ src/main/java/peaksoft/model/Deth8.java
@@ -1,4 +1,13 @@
 package peaksoft.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString(){
+        return " ";
+    }
+
 }
